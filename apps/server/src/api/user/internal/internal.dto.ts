0aa696e5317573647f90ee0a94eb0013d616e288
Jelegummykub <jelegummy.jrs@gmail.com>
2025-08-06 22:53:35 +0700

    init

--- apps/server/src/api/user/internal/internal.dto.ts
+++ apps/server/src/api/user/internal/internal.dto.ts
@@ -1,18 +1,1 @@
-import { createZodDto, patchNestJsSwagger } from 'nestjs-zod'
-import { z } from 'zod'
-
-export class UpdateUserArgs extends createZodDto(
-  z.object({
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-  }),
-) { }
-
-export class UpdatePasswordArgs extends createZodDto(
-  z.object({
-    oldpassword: z.string().min(8),
-    newPassword: z.string().min(8),
-  }),
-) { }
-
-patchNestJsSwagger()
+//TODO :
